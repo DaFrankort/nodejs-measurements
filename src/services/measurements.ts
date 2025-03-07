f00b11df8fa04ba52286583fa3f392836d978696
FrankortD <163832073+FrankortD@users.noreply.github.com>
2025-03-07 20:01:29 +0100

    Add MeasurementQueryBuilder for code clarity

--- src/services/measurements.ts
+++ src/services/measurements.ts
@@ -1,6 +1,6 @@
 import { Database } from "sqlite3";
 import { Measurement, MeasurementFilter, MeasurementStats } from "../types/measurement";
-import { measurementTable } from "../models/measurement";
+import { MeasurementQueryBuilder } from "../models/measurement";
 
 export class MeasurementService {
   private db: Database;
@@ -15,10 +15,10 @@ export class MeasurementService {
   public async create(measurement: Measurement): Promise<void> {
     console.log("Creating measurement:", measurement);
 
-    const query = `INSERT INTO ${measurementTable.name} (id, timestamp, value, meterID, type) VALUES (?, ?, ?, ?, ?);`;
-    const values = [measurement.id, measurement.timestamp, measurement.value, measurement.meterID, measurement.type];
+    const queryBuilder: MeasurementQueryBuilder = new MeasurementQueryBuilder();
+    queryBuilder.buildMeasurementInsert(measurement);
 
-    this.db.run(query, values), // TODO Return as promise, like the others
+    this.db.run(queryBuilder.query, queryBuilder.values), // TODO Return as promise, like the others
       (err: Error | null) => {
         if (err) {
           console.error("Error inserting data:", err.message);
@@ -33,26 +33,26 @@ export class MeasurementService {
   public async createMany(measurements: Measurement[]): Promise<void> {
     console.log(`Creating ${measurements.length} measurements`);
 
-    const query = `INSERT INTO ${measurementTable.name} (id, timestamp, value, meterID, type) VALUES (?, ?, ?, ?, ?);`;
+    const queryBuilder: MeasurementQueryBuilder = new MeasurementQueryBuilder();
+    queryBuilder.buildMeasurementInsert(measurements[0]); // Need to create base query to create statement
 
     return new Promise((resolve, reject) => {
       this.db.serialize(() => {
-        const statement = this.db.prepare(query);
+        const statement = this.db.prepare(queryBuilder.query);
         const promises: Array<Promise<void>> = [];
 
         for (const measurement of measurements) {
+          queryBuilder.buildMeasurementInsert(measurement);
+
           const promise = new Promise<void>((resolve, reject) => {
-            statement.run(
-              [measurement.id, measurement.timestamp, measurement.value, measurement.meterID, measurement.type],
-              (err: Error | null) => {
-                if (err) {
-                  console.error("Error inserting measurement:", err.message);
-                  reject(err);
-                  return;
-                }
-                resolve();
+            statement.run(queryBuilder.values, (err: Error | null) => {
+              if (err) {
+                console.error("Error inserting measurement:", err.message);
+                reject(err);
+                return;
               }
-            );
+              resolve();
+            });
           });
 
           promises.push(promise);
@@ -80,26 +80,11 @@ export class MeasurementService {
   public async findAll(filter: MeasurementFilter): Promise<Array<Measurement>> {
     console.log("Finding measurements with filter:", filter);
 
-    let query = `SELECT * FROM ${measurementTable.name}`;
-    let queryValues: Array<any> = [];
+    const queryBuilder: MeasurementQueryBuilder = new MeasurementQueryBuilder();
+    queryBuilder.buildMeasurementSelect(filter);
 
-    // Apply filters
-    const filterQueryParts = this.buildFilterQuery(filter);
-    if (filterQueryParts.conditions.length > 0) {
-      query += " WHERE " + filterQueryParts.conditions.join(" AND ");
-      queryValues.push(...filterQueryParts.values);
-    }
-
-    // Apply pagination
-    const page = filter.page ? filter.page : 1; // Default to page 1
-    const limit = filter.limit ? filter.limit : 50; // Default to 50 results per page
-    const offset = (page - 1) * limit;
-    query += " LIMIT ? OFFSET ?";
-    queryValues.push(limit, offset);
-
-    console.log("Executing query:", query, "with params:", queryValues);
     return new Promise((resolve, reject) => {
-      this.db.all(query, queryValues, (err: Error | null, rows: Array<Measurement>) => {
+      this.db.all(queryBuilder.query, queryBuilder.values, (err: Error | null, rows: Array<Measurement>) => {
         if (err) {
           console.error("Error selecting data:", err.message);
           reject(err);
@@ -115,18 +100,12 @@ export class MeasurementService {
    */
   public async getStats(filter: MeasurementFilter): Promise<MeasurementStats> {
     console.log("Calculating stats with filter:", filter);
-    let query = `SELECT COUNT(*) AS count, SUM(value) AS sum, AVG(value) AS average, MIN(value) AS min, MAX(value) AS max FROM ${measurementTable.name}`;
-    let queryValues: Array<any> = [];
 
-    // Apply filters
-    const filterQueryParts = this.buildFilterQuery(filter); // TODO -> Refactor into a Query Class (?)
-    if (filterQueryParts.conditions.length > 0) {
-      query += " WHERE " + filterQueryParts.conditions.join(" AND ");
-      queryValues.push(...filterQueryParts.values);
-    }
+    const queryBuilder: MeasurementQueryBuilder = new MeasurementQueryBuilder();
+    queryBuilder.buildStatsSelect(filter);
 
     return new Promise((resolve, reject) => {
-      this.db.get(query, queryValues, (err: Error | null, row: any) => {
+      this.db.get(queryBuilder.query, queryBuilder.values, (err: Error | null, row: any) => {
         if (err) {
           console.error("Error selecting data:", err.message);
           reject(err);
@@ -142,29 +121,5 @@ export class MeasurementService {
         });
       });
     });
-  }
-
-  /**
-   * Helper functions
-   */
-  private buildFilterQuery(filter: MeasurementFilter): { conditions: string[]; values: any[] } {
-    const conditions: string[] = [];
-    const values: any[] = [];
-
-    const filterMapping: { [key: string]: any } = {
-      "timestamp >= ?": filter.startDate,
-      "timestamp <= ?": filter.endDate,
-      "meterID = ?": filter.meterID,
-      "type = ?": filter.type,
-    };
-
-    for (const [condition, value] of Object.entries(filterMapping)) {
-      if (value !== undefined && value !== null) {
-        conditions.push(condition);
-        values.push(value);
-      }
-    }
-
-    return { conditions, values };
   }
 }
